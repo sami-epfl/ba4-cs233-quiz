901f52c1278d2c56821ac2bf53cb020e9ea6d437
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-04 22:51:31 +0200

    docs: change example with online engine

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 const QUIZ_CONFIG = {
   title: "COURSE-CODE",
   subtitle: "Course full name",
-  engineCdn: "https://cdn.jsdelivr.net/gh/YOUR-USER/quiz-engine@main",
+  engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
   engineLocal: "../quiz-engine",
 };
