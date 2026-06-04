e222ba3da799e6175f751f813ba7d02fa2be07db
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-04 22:30:43 +0200

    chore: add config example

---
+++ config.example.js
@@ -0,0 +1,6 @@
+const QUIZ_CONFIG = {
+  title: "COURSE-CODE",
+  subtitle: "Course full name",
+  engineCdn: "https://cdn.jsdelivr.net/gh/YOUR-USER/quiz-engine@main",
+  engineLocal: "../quiz-engine",
+};
