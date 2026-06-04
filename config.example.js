078844c0f1d2d9da3741fea1b8e986176fc60fa0
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-04 22:56:02 +0200

    chore: move config example to the engine
    fix: add config for online page

--- config.example.js
+++
@@ -1,6 +0,0 @@
-const QUIZ_CONFIG = {
-  title: "COURSE-CODE",
-  subtitle: "Course full name",
-  engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
-  engineLocal: "../quiz-engine",
-};
---
+++ config.js
@@ -0,0 +1,6 @@
+const QUIZ_CONFIG = {
+  title: "CS-233",
+  subtitle: "Introduction to Machine Learning",
+  engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
+  engineLocal: "../ba4-quiz-engine",
+};
