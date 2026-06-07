42b371c81dccc5c36eadfa20b9cad2d32c103fe8
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-07 19:28:29 +0200

    chore: update title config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const QUIZ_CONFIG = {
-  title: "CS-233",
+  title: "CS-233: Quizzes",
   subtitle: "Introduction to Machine Learning",
   engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
   engineLocal: "../ba4-quiz-engine",
