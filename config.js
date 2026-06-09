35852a608a5887506ae4706fc1908e381bd8269a
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-09 23:17:48 +0200

    feat: update config with last-commit date feature

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const QUIZ_CONFIG = {
   engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
   engineLocal: "../ba4-quiz-engine",
   landingUrl: "https://sami-epfl.github.io/ba4-quizzes/",
+  repoUrl: "https://github.com/sami-epfl/ba4-cs233-quiz",
 };
 // Use local engine when running via file:// or localhost
 QUIZ_CONFIG._useLocal = location.protocol === "file:" || location.hostname === "localhost" || location.hostname === "127.0.0.1";
